a20c7f45b6329a38a8f967679effa7b61c252c23
Jacob Martinez <jaccob.alxndr@gmail.com>
2021-01-04 11:38:13 -0800

    updating build dir

---
+++ build/precache-manifest.607c8c7697e9f911940c600e206fa257.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71de1dcd68e98ff52acb2655ec75c31e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fdd5a09d45e5fb20d98",
+    "url": "/static/css/main.ff0b6def.chunk.css"
+  },
+  {
+    "revision": "80b4a51a9386531429e6",
+    "url": "/static/js/2.325bfd55.chunk.js"
+  },
+  {
+    "revision": "3b84f1d6ff4918d7f6da7792a05457b8",
+    "url": "/static/js/2.325bfd55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdd5a09d45e5fb20d98",
+    "url": "/static/js/main.93311ccb.chunk.js"
+  },
+  {
+    "revision": "22cf7d7426dc07f57b93",
+    "url": "/static/js/runtime-main.46079b1d.js"
+  },
+  {
+    "revision": "c6f6eadd9c6c7f164f7c3a15de4c56f6",
+    "url": "/static/media/Bould-Bold.c6f6eadd.otf"
+  },
+  {
+    "revision": "3fc7e24755275c1b69484c31d136a384",
+    "url": "/static/media/Bould-Medium.3fc7e247.otf"
+  },
+  {
+    "revision": "d0b9b4fe6b4558a596b44368c3f7883b",
+    "url": "/static/media/Bould-Regular.d0b9b4fe.otf"
+  },
+  {
+    "revision": "b499461fd9faa38a32e80043829537fc",
+    "url": "/static/media/Bould-Thin.b499461f.otf"
+  }
+]);
